103ce9d37b380504cb2fd518083b05ceaf96abe0
Benjamin Clarke <benjaminclarke@Benjamin-Clarkes-MacBook-Pro.local>
2015-04-03 19:09:52 +0100

    Testing Github Features

--- accountManagement.py
+++ accountManagement.py
@@ -4,7 +4,7 @@ class accountManagement:
 
 	def add_new_account(database, fname, sname, pw, role):
 		password = login.hashPassword(pw)
-		acountRole = database.c.execute("SELECT roleID FROM role WHERE \'accountRole\' =" + role)
+		acountRole = database.c.execute("SELECT roleID FROM role WHERE \'accountRole\' =" + role)dkjdkjd
 		print(accountRole)
 		# database.c.execute("INSERT INTO account (surname,forname,salt,password) \
 		# 	VALUES (\'" + sname + "\',\'" + fname + "\',\'" + password['salt'] + "\',\'", + password['password'] + "\')")
